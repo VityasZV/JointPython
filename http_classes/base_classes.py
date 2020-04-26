3fae2e9837f90431e094d26177c13e7378fb8839
Victor Zadyabin <viktor.zadyabin@mail.ru>
2020-04-26 12:14:27 +0300

    classes Client, TokensConn, Cursor, Users

--- http_classes/base_classes.py
+++ http_classes/base_classes.py
@@ -1,8 +1,32 @@
 from _collections import defaultdict
 import psycopg2
 from psycopg2 import pool
+import logging
 
 __all__ = ['TokenConn', 'Reciever', 'ChatGroups']
+
+from http_classes.http_classes import HTTPError
+
+logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
+                    level=logging.DEBUG)
+
+
+class Users(object):
+    pass
+
+
+class Cursor:
+    def __init__(self, conn):
+        self.cursor = conn.cursor()
+
+    def __del__(self):
+        self.cursor.close()
+
+    def __enter__(self):
+        return self.cursor
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.cursor.close()
 
 
 class TokenConn:
@@ -43,8 +67,9 @@ class TokenConn:
     def set_cursor(self):
         self._cursor = self.conn.cursor()
 
-    def delete_token_from_user(self, users):
-        users[self.login]["auth_token"] = None
+    def delete_token_from_user(self, users: Users):
+        users[self.login].auth_token = None
+
     def __enter__(self):
         return self
 
@@ -54,12 +79,37 @@ class TokenConn:
             raise exc_val
 
 
-class Reciever:
+class TokensConn:
+    def __init__(self):
+        self.tokens_conn = dict()
 
-    '''
+    def __setitem__(self, key, value):
+        pass
+
+    def __getitem__(self, item: str):
+        if self.tokens_conn.get(item):
+            return self.tokens_conn[item]
+        else:
+            return None
+
+    def __delitem__(self, token):
+        self.tokens_conn.pop(token)
+
+    def delete_token_from_user(self, token: str, users: Users):
+        if self[token] is None:
+            raise HTTPError(404, 'Not found')
+        self[token].delete_token_from_user(users)
+        print(self.tokens_conn[token].login)
+        users[self.tokens_conn[token].login].connection = None
+        del self[token]
+
+
+class Reciever:
+    """
         i will iterate through users dict by logins of recievers in chats,
         trying to find needed address for sending message
-    '''
+    """
+
     def __init__(self, login):
         self.login = login
 
@@ -83,7 +133,6 @@ class ChatGroups:
     def __delitem__(self, chat_name):
         del self.chat_groups[chat_name]
 
-
     @property
     def cursor(self):
         return self._cursor
@@ -103,3 +152,52 @@ class ChatGroups:
 
     def set_cursor(self):
         self._cursor = self._conn.cursor()
+
+
+class Client:
+    def __init__(self, login: str, name: str, password: str):
+        self.login = login
+        self.name = name
+        self.password = password
+        self.auth_token = None
+        self.connection = None
+
+    def logout(self):
+        self.connection = None
+
+
+class Users:
+
+    def __init__(self, conn):
+        self.users = {}
+        self.conn = conn
+        cursor = self.conn.cursor()
+        cursor.execute('SELECT * FROM users')
+        records = cursor.fetchall()
+        cursor.close()
+        for (login, name, password) in records:
+            self.users[login] = Client(login, name, password)
+
+    def __getitem__(self, login) -> Client or None:
+        if self.users.get(login):
+            return self.users[login]
+        else:
+            return None
+
+    def __setitem__(self, login, user: Client):
+        if self[login] is None:
+            self.users[login] = user
+            with Cursor(self.conn) as cursor:
+                cursor.execute(f"INSERT INTO users "
+                               f"SELECT '{user.login}', '{user.name}', '{user.password}';")
+                self.conn.commit()
+                count = cursor.rowcount
+                logging.info(f'insert {count} values into users')
+                if count == 0:
+                    raise HTTPError(500, "user created locally but not inserted into database")
+        else:
+            raise HTTPError(405, "This login is already in use")
+
+    def __delitem__(self, chat_name):
+        # хендлера на удаление пользователя нет, если нужно удаляется руками через базу.
+        pass
