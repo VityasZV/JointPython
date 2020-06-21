fd390b970a55dca9f53d10424110d988fe81bdec
zvonand <s02170105@stud.cs.msu.ru>
2020-06-21 23:53:53 +0300

    Added wheel

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='PyChat',
-    version='1.0',
-    packages=['client', 'server', 'http_classes', "gui_templates"],
-    url='https://github.com/VityasZV/JointPython/',
+    version='',
+    packages=['client', 'server', 'http_classes', 'gui_templates'],
+    url='',
     license='WTFPL',
     author='JointPythonTeam',
-    description='Small chat',
-    install_requires=["PyQt5", "psycopg2", "urllib3", "requests"],
+    author_email='',
+    description='',
+    requires=["PyQt5", "psycopg2", "urllib3", "requests"]
 )
